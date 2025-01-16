030ca1a9e1a12a809c62b6c93bc8e44b86768f74
rina <k@rina.fyi>
2025-01-16 21:29:25 +1000

    recuresion

--- make.py
+++ make.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 
 import re
+import sys
 import subprocess
 import textwrap
 import shlex
@@ -72,7 +73,7 @@ def explore(cmd: list[str]):
   #   indent = len(l) - len(l.lstrip())
   #   indent //= 2
   #   print(indent, l)
-  
+
   # https://stackoverflow.com/questions/61464503/python-re-library-string-split-but-keep-the-delimiters-separators-as-part-of-the
   dash_options = (re.split(r'^(?=\s+-\S)', help, flags=re.MULTILINE))
 
@@ -87,7 +88,7 @@ def explore(cmd: list[str]):
     l = l.strip()
     if not l: continue
     name, desc = l.split(maxsplit=1)
-    if name == 'help': 
+    if name == 'help':
       subcmd = Subcommand('help', 'Print this message or the help of the given subcommand(s)', [], [], [])
     else:
       subcmd = explore(cmd + [name])
@@ -106,6 +107,7 @@ def explore(cmd: list[str]):
   return Subcommand(cmd[-1], '', dashes, subs, args)
 
 def make_arg(arg: str):
+  print(arg, file=sys.stderr)
   if arg.startswith('['):
     return f'-a {quote(quote(arg))}'
   elif arg.startswith('<'):
@@ -118,9 +120,9 @@ def suggest(suggestion: str, desc: str | None = None):
     s += quote('\t'+desc)
   return quote(f'{s}\n')
 
-def make_fish_completion(data: Subcommand):
+def make_fish_completion(data: Subcommand, prefix: str):
   join = shlex.join
-  cmd = data.cmd
+  cmd = prefix + data.cmd
   for dash in data.dashes:
     shorts = ''.join(join(['-s', a.lstrip('-')]) for a in dash.shorts)
     longs = ''.join(join(['-l', a.lstrip('-')]) for a in dash.longs)
@@ -130,11 +132,19 @@ def make_fish_completion(data: Subcommand):
 
     print(f'complete -c {cmd} -f {shorts} {longs} -d {desc} {args}')
 
-  # if data.subs:
-  #   print(f'complete -c {cmd} -f -a {''.join(suggest(x.cmd, x.desc) for x in data.subs)}')
-  print(f'complete -c {cmd} -f -a {quote('(_myfish_complete_subcommand --fcs-set-argv0="git checkout")')}')
+  if data.subs:
+    allsubs = quote(join([x.cmd for x in data.subs]))
+    print(f'complete -c {cmd} -f --condition "not __fish_seen_subcommand_from "{allsubs} -a {''.join(suggest(x.cmd, x.desc) for x in data.subs)}')
+    for sub in data.subs:
+      print(f'''complete -c {cmd} -f --condition "__fish_seen_subcommand_from {sub.cmd}" -a '(_myfish_complete_subcommand --fcs-set-argv0="{cmd}__{sub.cmd}")' ''')
+
+  for sub in data.subs:
+    print('')
+    make_fish_completion(sub, cmd + '__')
+
+  # print(f'complete -c {cmd} -f -a {quote('(_myfish_complete_subcommand --fcs-set-argv0="git checkout")')}')
 
 if __name__ == '__main__':
   cmds = explore(['git-branchless'])
   # pprint(cmds, width=140)
-  make_fish_completion(cmds)
+  make_fish_completion(cmds, '')
